890e4adaf897bee3644f7068a59779dc30180943
Serj <serhii.nanai@raccoongang.com>
2024-03-09 15:23:30 +0200

    implement regression

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+import pickle
+import numpy as np
 from flask import Flask, render_template, request
 
 app = Flask('lab4')
+FILENAME = 'diabetes_model.sav'
 
 
 @app.route('/')
@@ -12,10 +15,11 @@ def show_start_form():
 def result():
     form = request.form
     if request.method == 'POST':
-        number = request.form['number']
-        fib = lambda n: fib(n - 1) + fib(n - 2) if n > 2 else 1
-        predict = fib(int(number))
-    return render_template('resultsform.html', number=number, predicted_number=predict)
+        x = request.form['number']
+        value = np.array([float(x)])
+        loaded_model = pickle.load(open(str(FILENAME), 'rb'))
+        predicted_y = loaded_model.predict(value.reshape(1, -1))
+        return render_template('resultsform.html', number=x, predicted_number=predicted_y[0][0])
 
 
 app.run()
